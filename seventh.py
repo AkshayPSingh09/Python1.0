279e175172d6fc2c5d7b0a125e981745e6ba82be
AkshayPSingh09 <ap321go@gmail.com>
2021-11-13 02:18:09 -0800

    commit second branch changes

--- seventh.py
+++ seventh.py
@@ -3,5 +3,5 @@
 print('This is a string {}'.format("Inserted"))
 
 print('Once upon a time,\n there was {2} who used to come late to the school,\n the teacher used to scold him.\n His friend {1} used to save him from getting scolded,\nThey fought with {0} together'.format('Ram','Shayam','Tarun'))
-
-print()
+v = "this is a fine line b/w git and me"
+print(v)
