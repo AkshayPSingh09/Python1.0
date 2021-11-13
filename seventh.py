b05d8a1368b9ec6067ed976b054ce77a55fa801b
AkshayPSingh09 <ap321go@gmail.com>
2021-11-13 01:57:12 -0800

    first commit

---
+++ seventh.py
@@ -0,0 +1,7 @@
+#we can insert text in string using format
+
+print('This is a string {}'.format("Inserted"))
+
+print('Once upon a time,\n there was {2} who used to come late to the school,\n the teacher used to scold him.\n His friend {1} used to save him from getting scolded,\nThey fought with {0} together'.format('Ram','Shayam','Tarun'))
+
+print()
